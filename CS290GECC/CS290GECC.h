27af16474ec76608b830de96e333ab43a4d43c3e
Johan Henkens <johan@henkens.com>
2013-05-11 13:06:45 -0700

    Initial Commit

---
+++ CS290GECC/CS290GECC.h
@@ -0,0 +1,13 @@
+//
+//  CS290GECC.h
+//  CS290GECC
+//
+//  Created by Johan Henkens on 5/11/13.
+//  Copyright (c) 2013 Johan Henkens. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CS290GECC : NSObject
+
+@end
---
+++ CS290GECC/CS290GECC.m
@@ -0,0 +1,13 @@
+//
+//  CS290GECC.m
+//  CS290GECC
+//
+//  Created by Johan Henkens on 5/11/13.
+//  Copyright (c) 2013 Johan Henkens. All rights reserved.
+//
+
+#import "CS290GECC.h"
+
+@implementation CS290GECC
+
+@end
